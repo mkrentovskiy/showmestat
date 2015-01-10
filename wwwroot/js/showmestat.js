4488cfec1df458024843c5cee10bd2458a864f4e
Maxim Krentovskiy <mkrentovskiy@gmail.com>
2015-01-11 01:13:22 +0300

    Done with the flows demonstration. Add icomoon font and png.

--- wwwroot/js/showmestat.js
+++ wwwroot/js/showmestat.js
@@ -1,9 +1,12 @@
 (function($) {
     var ws = null;
     var repeat = function(f) { setTimeout(f, 1000) };
-    
+    var t = {};
+
     $.app = {}
     $.app.init = function() {
+        ui_init();
+
         window.WebSocket = window.WebSocket || window.MozWebSocket;
         connect(true);
     }
@@ -30,23 +33,35 @@
         }
         ws.onmessage = function(m) { 
             if(m.data) { 
-                obj = JSON && JSON.parse(m.data) || $.parseJSON(m.data);
-                console.log(obj);
-            } 
+                var obj = JSON && JSON.parse(m.data) || $.parseJSON(m.data);
+                var cb = (obj.req && obj.req.length) ? function() { repeat(function(){ get(obj.req); }); } : null;
+                if(obj.reply && obj.reply.length) 
+                    ui_apply(obj.req, proto_parse(obj.reply), cb);
+            }             
         }
     }
-
-    function get(t) {
-        if(ws.readyState == ws.OPEN) {
-            ws.send(t);
-        } 
-        repeat(function(){ get(t); });
-    }       
+    
+    function get(t) { if(ws.readyState == ws.OPEN) ws.send(t); }       
+    function proto_parse(str) { return $.map(str.split(';'), function(v, n) { return Array(v.split('|')); }); }
 
     /*
         UI
     */
+
+    function ui_init() {
+        $.map($("._template"), function(v, i) { t[$(v).attr('id')] = $(v).html(); });
+    };
+
     function ui_con_state(state) { $(".con_state").hide(); $(".con_state_" + state).show(); }
+    function ui_apply(cl, data, cb) {
+        $("." + cl).html($.map(data, function(v, k) { return ui_apply_template(cl, v); }).join(''));
+        // if(cb) cb();
+    }
+
+    function ui_apply_template(cl, data) {        
+        return data.reduce(function(prev, curr, i, a) { return prev.replace(new RegExp("@" + i + "@", 'g'), curr); }, t[cl]);
+    }
+
 
 })(window.jQuery);
 
